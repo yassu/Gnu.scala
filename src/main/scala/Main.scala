924f18e23caa5d5d396245248e1144f0b2cf758a
yassu <yassu0320.dev@gmail.com>
2017-11-12 00:37:42 +0900

    ExecuteItemList.writeInto

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,15 @@
+import gnu.configuration.XRangeConfiguration
+import gnu.core.ExecuteItem
+import gnu.core.ExecuteItemList
+import gnu.core.Plotter
+import java.io.File
+
 object Main extends App {
-  println("Hello World!")
+  val l = new ExecuteItemList(List(
+    new Plotter("x * x"),
+    new XRangeConfiguration(Some(-1), Some(1))
+  ))
+
+  println(l.toString)
+  l.writeInto(new File("test.gnu"))
 }
--- src/main/scala/core/ExecuteItemList.scala
+++ src/main/scala/core/ExecuteItemList.scala
@@ -1,7 +1,21 @@
 package gnu.core
 
 import gnu.core.ExecuteItem
+import java.io._
 
 class ExecuteItemList(val execList: List[ExecuteItem]) {
+  private def printToFile(f: File)(op: PrintWriter => Unit) {
+    val p = new PrintWriter(f)
+    try {
+      op(p)
+    } finally {
+      p.close()
+    }
+  }
+
+  def writeInto(f: File) {
+    printToFile(f) {p => p.println(this.toString)}
+  }
+
   override def toString:String = execList.map(e => e.executeString).mkString("\n")
 }

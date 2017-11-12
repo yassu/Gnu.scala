f635709393bf5cb7d66984fa53900870b3e7417a
yassu <yassu0320.dev@gmail.com>
2017-11-12 17:50:32 +0900

    range config

--- src/main/scala/core/plotter/Plotter.scala
+++ src/main/scala/core/plotter/Plotter.scala
@@ -2,6 +2,27 @@ package gnu.core.plotter
 
 import gnu.core.ExecuteItem
 
-class Plotter(private val formula: String) extends ExecuteItem {
-  def executeString:String = s"plot ${formula}"
+class Plotter(private val formula: String,
+    private val xMin:Option[Double]=None, private val xMax:Option[Double]=None
+    ) extends ExecuteItem {
+
+  def rangeString:String = if (xMin == None && xMax == None) "" else s"[%s:%s]".format(
+    xMin match {
+      case Some(x) => x.toString
+      case None => ""
+    },
+    xMax match {
+      case Some(x) => x.toString
+      case None => ""
+    }
+  )
+  def executeString:String = {
+    var rangeString = this.rangeString
+
+    if (rangeString.isEmpty) {
+      s"plot ${formula}"
+    } else {
+      s"plot ${rangeString} ${formula}"
+    }
+  }
 }
--- src/test/scala/core/plotter/PlotterSpec.scala
+++ src/test/scala/core/plotter/PlotterSpec.scala
@@ -9,8 +9,34 @@ class PlotterSpec extends org.specs2.mutable.Specification {
 
     1 must_== 1
   }
+  "#optionString" >> {
+    "Neither is set" >> {
+      new Plotter("x*x").rangeString must_== ""
+    }
+    "Just Left is set" >> {
+      new Plotter("x*x", xMin=Some(-1.0)).rangeString must_== "[-1.0:]"
+    }
+    "Just Right is set" >> {
+      new Plotter("x*x", xMax=Some(1.0)).rangeString must_== "[:1.0]"
+    }
+    "Both are set" >> {
+      new Plotter("x*x", xMin=Some(-1.0), xMax=Some(1.0)).rangeString must_== "[-1.0:1.0]"
+    }
+  }
   "#executeString" >> {
-    new Plotter("x*x").executeString must_== "plot x*x"
+    "Neither is set" >> {
+      new Plotter("x*x").executeString must_== "plot x*x"
+    }
+    "Just Left is set" >> {
+      new Plotter("x*x", xMin=Some(-1.0)).executeString must_== "plot [-1.0:] x*x"
+    }
+    "Just Right is set" >> {
+      new Plotter("x*x", xMax=Some(1.0)).executeString must_== "plot [:1.0] x*x"
+    }
+    "Both are set" >> {
+      new Plotter("x*x", xMin=Some(-1.0), xMax=Some(1.0)).executeString must_==
+        "plot [-1.0:1.0] x*x"
+    }
   }
 }
 
